5a411f4107d6bfde9159f308412b94aa1afe9bd7
Mario Salazar <mariosalazar.ms.10@gmail.com>
2025-10-26 06:08:15 -0500

    feat(property): add project structure for property microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app/controller/app.controller';
+import { AppService } from './app/service/app.service';
+import { HomeModule } from './app/module/home.module';
+import { AppPropertyModulesUsingPostgreSQL } from './factory/postgresql/modules-using-postgresql.module';
 
 @Module({
-  imports: [],
+  imports: [HomeModule, AppPropertyModulesUsingPostgreSQL],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
